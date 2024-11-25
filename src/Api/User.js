543806d21b57ef7483663bd95335f476b2da32ad
Purplebelt1 <wdeb2013@gmail.com>
2024-11-25 09:33:09 -0600

    Fixed login

---
+++ src/Api/User.js
@@ -0,0 +1,22 @@
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from '../Utils/UserContext';
+
+const url = 'http://localhost:5276/api/users/';
+const getCommonHeader = (token) => ({
+    headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    },
+});
+
+export const getUserAPI = async (username, token) => {
+    try {
+        const response = await fetch(`${url}username/${username}`, getCommonHeader(token));
+        const data = await response.json();
+        console.log(data)
+        return data || null;
+    } catch {
+        console.error('Error fetching user');
+        return null;
+    }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
+import React from "react";
 import Navbar from "./Components/NavBar";
 import HomePage from "./Pages/HomePage";
 import Nutrition from "./Pages/Nutrition";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 import { Container } from "reactstrap";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-import './App.css'
+import { UserContext, UserProvider } from "./Utils/UserContext";
+import './App.css';
 
 function App() {
-
   return (
     <Router>
-      <Navbar />
-      <Container className="mainContainer">
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/nutrition" element={<Nutrition />} />
-        </Routes>
-      </Container>
+      <UserProvider>
+        <MainApp />
+      </UserProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+function MainApp() {
+  const { currentUser: user } = React.useContext(UserContext);
+
+
+  return (
+    <>
+      {user && <Navbar />}
+      <Container className="mainContainer">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {user ? (
+            <>
+              <Route exact path="/" element={<HomePage />} />
+              <Route path="/nutrition" element={<Nutrition />} />
+            </>
+          ) : (
+            <Route path="/*" element={<Navigate to="/login" replace />} />
+          )}
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
